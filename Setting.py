2bab5d2769f15cfb35d808f90ab01736af3221f1
draeson <Draesonfang@gmail.com>
2024-11-04 19:54:13 +0000

    Optimised the overall process

--- Setting.py
+++ Setting.py
@@ -2,8 +2,12 @@
 This is all the parameters needed for this mission
 '''
 security_van_capacity = 0 #security van capacity
-population_size = 0
+population_size = 70
 pheromone = 0
-evaporation_rate = 0.75
+evaporation_rate = 0.6
+alpha = 1
+beta = 2
+random_seed =900
+iters = 145
 
 
--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 ECMM409 – Nature-Inspired Computation
 COURSEWORK
 '''
+from docutils.nodes import label
+from holoviews.plotting.bokeh.styles import marker
 from sympy.stats.rv import probability
 
-from Setting import security_van_capacity, population_size, pheromone, evaporation_rate
+from Setting import security_van_capacity, population_size, iters, evaporation_rate, alpha, beta, random_seed, pheromone
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 bag_data = []
 
-#template parameter,temp is used to store the value of capacity,weight,value etc,tempDict is used to store a dictionary include weight and value
+# template parameter,temp is used to store the value of capacity,weight,value etc,tempDict is used to store a dictionary include weight and value
 temp = 0
 tempDict = {}
 
@@ -38,86 +40,178 @@ with open('BankProblem.txt', 'r') as f:
     # print(bag_data)
 
 data = pd.DataFrame(bag_data)
-# print(data)
-
 
 # create a value matrix in shape (10,10)
 value_matrix = np.array(data[['value']].values.reshape(100))
 # create a weight matrix in shape (10,10)
 weight_matrix = np.array(data[['weight']].values.reshape(100))
 
-# print(value_matrix)
-# print(weight_matrix)
-# create a heuristic matrix, base on inverse of value / weight, which means the smaller the value the better the characterisation
-heuristic_matrix = np.array([round(1/value_matrix[i]/weight_matrix[i],4) for i in range(100)])
-# print(heuristic_matrix)
+value_weight_ratio = value_matrix / weight_matrix
 
-# create a random seed to initialise pheromone matrix
-# np.random.seed(77)
-p_seed = np.random.rand()
 
-# create a pheromone matrix filling with random seed in initialization
-pheromone_matrix = np.full(100,p_seed)
-
-# print(pheromone_matrix)
-# remove node
-# using log to cumsum
-def chose_path(alpha,beta,heru_matric,phrm_matrix,bag):
+def chose_path(alpha, beta, heru_matric, phrm_matrix, bag):
     heru_matric[bag] = 0
     phrm_matrix[bag] = 0
-    total_value = np.array([phrm_matrix[i]**alpha*heru_matric[i]**beta for i in range(len(phrm_matrix))]).sum()
-    probability_matrix = np.array([phrm_matrix[i]**alpha*heru_matric[i]**beta / total_value for i in range(len(phrm_matrix))])
+    total_value = np.array([phrm_matrix[i] ** alpha * heru_matric[i] ** beta for i in range(len(phrm_matrix))]).sum()
+    probability_matrix = np.array(
+        [phrm_matrix[i] ** alpha * heru_matric[i] ** beta / total_value for i in range(len(phrm_matrix))])
     probability_matrix = np.cumsum(probability_matrix)
+    np.random.seed(None)
     choice_seed = np.random.rand()
-    for i,_ in enumerate(probability_matrix):
+    for i, _ in enumerate(probability_matrix):
         if (choice_seed <= probability_matrix[i]) and (i not in select_bag):
             return i
 
+
+# select final bag
+def chose_final_path(alpha, beta, heru_matric, phrm_matrix, temp_weight, bag):
+    heru_matric[bag] = 0
+    phrm_matrix[bag] = 0
+    heru_matric[weight_matrix > temp_weight] = 0
+    phrm_matrix[weight_matrix > temp_weight] = 0
+    total_values = np.array([phrm_matrix[i] ** alpha * heru_matric[i] ** beta for i in range(len(phrm_matrix))]).sum()
+    probability_matrix = np.array(
+        [phrm_matrix[i] ** alpha * heru_matric[i] ** beta / total_values for i in range(len(phrm_matrix))])
+    probability_matrix = np.cumsum(probability_matrix)
+    np.random.seed(None)
+    choice_seed = np.random.rand()
+    for i, _ in enumerate(probability_matrix):
+        if (choice_seed <= probability_matrix[i]) and (i not in select_bag):
+            return i
+
+    return -1
+
+
 # return a total value
 def fitness(slct_bag):
-    total_value = np.array([value_matrix[i] for i in slct_bag]).sum()
-    total_weight = np.array([weight_matrix[i] for i in slct_bag]).sum()
-    return total_value if total_weight < security_van_capacity else -1
+    total_val = np.array([value_matrix[i] for i in slct_bag]).sum()
 
-def update_phremone(ind,bags):
-    pheromone_matrix[ind] += fitness(bags)
+    return total_val
+
+
+def update_pheromone(bag, fitness_val, p_m, mode=1):
+    if mode == 1:
+        t_max = 5.0
+        t_min = 0.1
+    else:
+        t_max = 10.0
+        t_min = 0.01
+
+    p_m[bag] += fitness_val/500
+
+    return np.clip(p_m, t_min, t_max)
+
+
+def evaporate_pheromone():
     for _ in range(len(pheromone_matrix)):
-        pheromone_matrix[_] *= evaporation_rate
+        pheromone_matrix[_] *= (1-evaporation_rate)
 
-# print(value_matrix[value_matrix>security_van_capacity])
 
 '''
 Parameter Defination
 '''
 select_bag = np.array([])
 total_value = 0
-num_bags = 100
-num_ants = 20
-alpha = 1
-beta = 2
-for ant in range(num_ants):
-    select_bag = []
+total_weight = 0
 
-    total_value = 0
-    total_weight = 0
-    while True:
-        if total_weight> security_van_capacity:
-            select_bag = select_bag[:-1]
-            break
-        # print(pheromone_matrix)
-        ind = chose_path(alpha,beta,heuristic_matrix.copy(),pheromone_matrix.copy(),select_bag)
-        if ind is None:
-            break
-        select_bag.append(ind)
+global_best_bags = np.array([])
+global_best_value = 0
+
+clct_value = np.array([])
+clct_indicator = np.array([])
+best_ant_ind = 0
+
+heuristic_matrix = np.array([round((value_matrix[i] / weight_matrix[i]), 4) for i in range(100)])
+
+# create a random seed to initialise pheromone matrix
+np.random.seed(random_seed)
+p_seed = np.random.rand()
+
+# create a pheromone matrix filling with random seed in initialization
+pheromone_matrix = np.full(100, p_seed)
+for _ in range(iters):
+    iter_best_bag = np.array([])
+    iter_best_value = 0
+
+    for ant in range(population_size):
+        select_bag = np.array([], dtype=int)
+        total_value = 0
+        total_weight = 0
+
+        while True:
+            if total_weight > security_van_capacity:
+                select_bag = select_bag[:-1]
+                break
+            #choose a bag
+            ind = chose_path(alpha, beta, heuristic_matrix.copy(), pheromone_matrix.copy(), select_bag)
+            # if ind equal -1, which mean there is no bag that is selected
+            if ind == -1:
+                break
+
+            select_bag = np.append(select_bag, ind)
+            total_weight = weight_matrix[select_bag].sum()
+
+        # see if total wight bigger than van can carry
+        total_weight = weight_matrix[select_bag].sum()
+        temp_weight = security_van_capacity - total_weight
+
+        # check if there is any bag that van can carry
+        while temp_weight >= 1:
+            ind = chose_final_path(alpha, beta, heuristic_matrix.copy(), pheromone_matrix.copy(), temp_weight,
+                                   select_bag)
+            if ind == -1:
+                break
+            select_bag = np.append(select_bag, ind)
+            total_weight = weight_matrix[select_bag].sum()
+            temp_weight = security_van_capacity - total_weight
+
         total_value = fitness(select_bag)
         total_weight = weight_matrix[select_bag].sum()
-        update_phremone(ind,select_bag)
+        # iteration best value
+        if iter_best_value < total_value:
+            iter_best_value = total_value
+            iter_best_bag = select_bag
+        # Global best value
+        if global_best_value <= total_value:
+            global_best_value = total_value
+            global_best_bags = select_bag
+            best_ant_ind = _
+        clct_value = np.append(clct_value, total_value)
+        clct_indicator = np.append(clct_indicator, round(iter_best_value / weight_matrix[iter_best_bag].sum(), 4))
 
-    print('*'*100)
-    total_value = value_matrix[select_bag].sum()
-    print(select_bag)
-    print(total_value)
-    total_weight = weight_matrix[select_bag].sum()
-    print(total_weight)
-    print(security_van_capacity-total_weight)
-    print(round(total_value/total_weight,4))
+        # Pheromone update with iteration-best or global-best
+        if _ < iters / 2:
+            # Use iteration-best update in the first half of the iterations
+            # for _ in range(population_size):
+            pheromone_matrix = update_pheromone(iter_best_bag, iter_best_value, pheromone_matrix.copy(), 1)
+        else:
+            # Use global-best update in the second half of the iterations
+            # for _ in range(population_size):
+            pheromone_matrix = update_pheromone(global_best_bags, global_best_value, pheromone_matrix.copy(), 2)
+
+        evaporate_pheromone()
+
+print(global_best_bags)
+print(global_best_value)
+print(weight_matrix[global_best_bags].sum())
+print(round(global_best_value / weight_matrix[global_best_bags].sum(), 4))
+print(select_bag)
+print(total_value)
+print(total_weight)
+print(total_value / total_weight)
+
+# visualisation
+fig, ax = plt.subplots(2, 1, figure=(16, 10))
+ax[0].plot(range(len(clct_value)), clct_value, c='r', label='Total Value')
+ax[0].scatter(best_ant_ind, global_best_value, c='g', label='Best Value')
+ax[0].set_xlabel("Iteration")
+ax[0].set_ylabel("Total value")
+ax[0].set_title("Best Total value in each iteration")
+ax[0].legend()
+ax[1].plot(range(len(clct_indicator)), clct_indicator, c='b', label='Unit weight value')
+ax[1].set_xlabel("Iteration")
+ax[1].set_ylabel("Best value-to-weight-ratio")
+ax[1].set_title("Best Unit weight value in each iteration")
+ax[1].legend()
+plt.subplots_adjust(wspace=0.5, hspace=0.7)
+plt.show()
